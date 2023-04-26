2209ade84740a5ac630f47d8849ae21832f98847
Heungwan Kim <sseam.corp@gmail.com>
2023-04-27 00:40:59 +0900

    Add Ads

--- app/src/main/java/com/sseam/android/counter/config/Constants.kt
+++ app/src/main/java/com/sseam/android/counter/config/Constants.kt
@@ -2,7 +2,6 @@ package com.sseam.android.counter.config
 
 class Constants {
     companion object {
-        const val ADMOB_APP_ID = "ca-app-pub-9912818029252604~3769785000"
         const val BANNER_AD_UNIT_ID = "ca-app-pub-9912818029252604/6761396968"
         const val BANNER_AD_UNIT_ID_TEST = "ca-app-pub-3940256099942544/6300978111"
     }
